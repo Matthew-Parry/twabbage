d5585271afeeb1f4f06ce519a164cca7e1691391
becsalana <matthew@becsalana.co.uk>
2014-07-19 17:16:50 +0000

    Code updated to tweet image file now

--- twabbage.py
+++ twabbage.py
@@ -3,6 +3,8 @@ from time import sleep
 from datetime import datetime
 import os
 import RPi.GPIO as GPIO
+import tweepy
+import random
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -16,6 +18,34 @@ GPIO.setup(18, GPIO.OUT)
 #while True:
 
 def tweet(filename):
+    consumer_key = 'ACNq06IuOaApzHGHzJes4XIfz'
+    consumer_secret = '3bzuG0HuyUcEd5iiaM8ifO7PTajilqy0XvDVjoZUqZZF3dGQtL'
+    access_token = '2647226370-b4eJ4Inm4dRbRXFpdwDTbYNpjoRgroit4AQL2Hs'
+    access_token_secret = 'uLUs6bNRwQtWjhfM8FciROvGTAlPRUGVSF1Yn984id4wu'
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    api = tweepy.API(auth)
+
+    statuses = [
+        "Looking Good!",
+        "Wow! Nice outfit!",
+        "Oh dear, is it a bad hair day?",
+        "Stand still silly!",
+        "Twit-twoo!",
+        "Pi-tastic!",
+        "Babbage, babbage not on the wall, who is the fairest of them all?",
+        "Pray Mr Babbage, how do I look?",
+        "Perhaps you should get an early night this evening?!",
+        "STUNNING!",
+        "Ada Lovelace would be impressed!",
+        "This should be your profile picture!",
+    ]
+
+    photo_path =filename
+    status = random.choice(statuses) + " #picademy"
+    api.update_with_media(photo_path, status)
     print("Tweeted %s"% filename)
 
 
@@ -25,7 +55,7 @@ with picamera.PiCamera() as camera:
     camera.start_preview()
     
     #shutter button pressed
-#    GPIO.wait_for_edge(17, GPIO.FALLING)
+    GPIO.wait_for_edge(17, GPIO.FALLING)
     #led light in other eye   
 
     GPIO.output(18, 1)
@@ -52,7 +82,7 @@ with picamera.PiCamera() as camera:
     sleep(0.1)
     GPIO.output(18, 0)
     #shutter sound
-    os.system("aplay /home/pi/twabbage.wav")
+    os.system("aplay /home/pi/twabbage/twabbage.wav &")
 
     timenow=datetime.now()
     formatted_time = timenow.strftime("%Y-%m-%d-%H%M%S")
